9c1bd505110370955e07f8fc828711d06c7d2352
Adam Ahmetasevic <adamahmetasevic@gmail.com>
2024-04-27 16:26:26 -0500

    What is goijng on
    Jakarta error was caused by dual imports in pomxml. Now continuing to work on config

---
+++ BellsCargoProject/src/main/java/dmacc/config/SecurityConfig.java
@@ -0,0 +1,52 @@
+package dmacc.config;
+
+import dmacc.beans.UserDetailsServiceImplementation;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Autowired
+    private UserDetailsServiceImplementation userDetailsService;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+            .authorizeHttpRequests(authorize -> 
+                authorize
+                .requestMatchers("/login", "/css/**").permitAll() // Allow access to login page and CSS resources
+                .anyRequest().authenticated()
+            )
+            .formLogin(formLogin ->
+                formLogin
+                    .loginPage("/login") // Specify custom login page
+                    .permitAll() // Allow everyone to access the login page
+            )
+            .logout(logout ->
+                logout
+                    .permitAll()
+            );
+        return http.build();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+    }
+}
